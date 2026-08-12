c23992021fd2861b578f5d5da2d9d6bf051f8a81
ruanzadaa17-bit <ruanzadaa17@gmail.com>
2026-08-12 08:47:24 -0300

    Concluimos conceitos e atividades.

--- Aula-05/Exercício-06-07/ContaBancaria/obj/Debug/net10.0/ContaBancaria.AssemblyInfo.cs
+++ Aula-05/Exercício-06-07/ContaBancaria/obj/Debug/net10.0/ContaBancaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81889bc59721c18cf329775c43273e03bc9f42f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605a27c54dcb8648cb8727d78ad84d6028a9961b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaBancaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
