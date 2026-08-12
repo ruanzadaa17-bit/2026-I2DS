de6c8c64517d5deec42868fb267528f01471338f
ruanzadaa17-bit <ruanzadaa17@gmail.com>
2026-08-12 11:15:15 -0300

    Aula-06

---
+++ Aula-06/Filas/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM FILAS (FIFO) ===");
+
+        // 1 - Criar uma nova fila vazia
+        Queue<string> filaBanco = new Queue<string>();
+
+        // 2 - Adicionar elementos na fila
+        filaBanco.Enqueue("Zuleika");       // 0
+        filaBanco.Enqueue("Asdrúbal");      // 1
+        filaBanco.Enqueue("Pafúncio");      // 2
+        filaBanco.Enqueue("Epaminondas");   // 3
+
+        // 3 - Percorrer elementos de uma fila
+        Console.WriteLine();
+        foreach(var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // 4 - Retirar um elemento da lista
+        string pessoaRetirada = filaBanco.Dequeue();
+        Console.WriteLine();
+        Console.WriteLine($"O nome chamado foi: {pessoaRetirada}");
+
+        filaBanco.Enqueue("Lucrécia");
+        Console.WriteLine();
+        foreach(var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // 5 - Verificar se existe um determinado elemento na lista
+        string procurar = "Pafúncio";
+        bool achou = filaBanco.Contains(procurar);
+
+        if (achou)
+        {
+            Console.WriteLine($"{procurar} está na fila!");
+        }
+        else
+        {
+            Console.WriteLine($"{procurar} não está na fila");
+        }
+    }
+}
---
+++ Aula-06/Pilhas/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM PILHAS   (FILO) ===");
+
+        // 1 - Criar uma nova pilha vazia
+        Stack<string> pilhasLivros = new Stack<string>();
+
+        // 2 - Adicionar elementos em uma pilha
+        pilhasLivros.Push("Dom quixote");
+        pilhasLivros.Push("O Morro dos Ventos Uivantes");
+        pilhasLivros.Push("O Cortiço");
+
+        // 3 - Percorrer todos os elementos de uma pilha
+        Console.WriteLine();
+        foreach (var livro in pilhasLivros)
+        {
+            Console.WriteLine(livro);
+        }
+
+        // 4 - Remover um elemento da pilha
+        string livroRemovido = pilhasLivros.Pop();
+        Console.WriteLine();
+        Console.WriteLine($"O livro retirado foi: {livroRemovido}");
+
+        pilhasLivros.Push("Dom Casmurro");
+        Console.WriteLine();
+        Console.WriteLine();
+        foreach (var livro in pilhasLivros)
+        {
+            Console.WriteLine(livro);
+        }
+    }
+}
